10fe66527dad7155e6975e76b6e17be2bfe2be3a
Dexef <cut4pizza@gmail.com>
2021-10-08 11:10:17 +0200

    added a generic print vector function

--- algsmain.cpp
+++ algsmain.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 #include <math.h>
 
+template <typename T>
+void printVector(std::vector<T> toPrint){
+    for(int i = 0; i < toPrint.size(); i++){
+        std::cout << toPrint[i];
+    }
+    std::cout << std::endl;
+}
+
+
 int main(){
-    
+    std::vector<char> cringerz = {'b', 'a', 's', 'e', 'd' };
+    printVector(cringerz);
 }
